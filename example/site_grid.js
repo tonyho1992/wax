86a0eee35435c01d5bff7de2e3d962788582dde8
Tom MacWright <macwright@gmail.com>
2011-02-22 15:14:16 -0500

    Adding zany ascii grid example.

---
+++ example/site_grid.js
@@ -0,0 +1,22 @@
+var map;
+$(window).load(function() {
+    var l = new wax.g.ASCIIMapType();
+    map = new google.maps.Map(
+        document.getElementById("google-canvas"), {
+            center: new google.maps.LatLng(0, 0),
+            zoom: 2,
+            mapTypeControlOptions: {
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'mapbox']
+            }
+    });
+
+    map.mapTypes.set('mb', l);
+
+    // Set the default map to world light
+    map.setMapTypeId('mb');
+
+    // Add the MapBox logo to the map
+    // mbLogo(map);
+
+    wax.g.Interaction(map);
+});
