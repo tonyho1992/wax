6c20e87759f0d6b02e0cbf857061735cc07c3b98
Young Hahn <young@developmentseed.org>
2011-02-23 15:45:55 -0500

    Update legend control to be wax-able, support layer.json.

--- control/ol/legend.js
+++ control/ol/legend.js
@@ -6,7 +6,7 @@ var wax = wax || {};
 wax.ol = wax.ol || {};
 
 wax.ol.Legend = OpenLayers.Class(OpenLayers.Control, {
-    CLASS_NAME: 'OpenLayers.Control.Legend',
+    CLASS_NAME: 'wax.ol.Legend',
     container: null,
     attached: false,
 
@@ -23,11 +23,6 @@ wax.ol.Legend = OpenLayers.Class(OpenLayers.Control, {
             this.attached = true;
             $(this.map.viewPortDiv).append(this.container);
         }
-        for (var i = 0; i < this.map.layers.length; i++) {
-            var layer = this.map.layers[i];
-            this.setLegend(layer);
-            layer.events.register('visibilitychanged', layer, this.setLegend);
-        }
         return OpenLayers.Control.prototype.activate.apply(this, arguments);
     },
 
@@ -40,31 +35,46 @@ wax.ol.Legend = OpenLayers.Class(OpenLayers.Control, {
         return OpenLayers.Control.prototype.deactivate.apply(this, arguments);
     },
 
-    setLegend: function(layer) {
-        // The layer param may vary based on the context from which we are called.
-        layer = layer.object || layer;
-        var url = this.legendUrl(layer);
+    // Add handlers to the map
+    setMap: function(map) {
+      OpenLayers.Control.prototype.setMap.apply(this, arguments);
+      this.activate();
+      this.map.events.on({
+          "addlayer": this.setLegend,
+          "changelayer": this.setLegend,
+          "removelayer": this.setLegend,
+          "changebaselayer": this.setLegend,
+          scope: this
+      });
+    },
 
-        if ('legend' in layer) {
-            this.render(layer, layer.legend);
-        } else if (url) {
-            return $.jsonp({
-                'url': url,
-                context: this,
-                success: function(data) {
-                    if (data && data.legend) {
-                        layer.legend = data.legend;
-                        this.render(layer, layer.legend);
-                    }
-                },
-                error: function() {},
-                callback: layer.layername, // @TODO make callback safe
-                callbackParameter: 'callback'
-            });
+    setLegend: function(layer) {
+        for (var i = 0; i < this.map.layers.length; i++) {
+            var layer = this.map.layers[i],
+                url = this.legendUrl(layer);
+            if ('legend' in layer) {
+                this.render(layer);
+            } else if (url) {
+                return $.jsonp({
+                    'url': url,
+                    context: this,
+                    success: function(data) {
+                        (data && data.legend) && (layer.legend = data.legend);
+                        this.render(layer);
+                    },
+                    error: function() {
+                        layer.legend = false;
+                    },
+                    callback: layer.layername, // @TODO make callback safe
+                    callbackParameter: 'callback'
+                });
+            }
         }
     },
 
-    render: function(layer, legend) {
+    render: function(layer) {
+        // Layer has no legend. Do nothing.
+        if (!layer.legend) return;
         // Layer is visible but so is legend. Do nothing.
         if (layer.visibility && ('legendDiv' in layer)) return;
         // Hide legend if layer is no longer visible.
@@ -73,14 +83,14 @@ wax.ol.Legend = OpenLayers.Class(OpenLayers.Control, {
             delete layer.legendDiv;
             return;
         }
-        layer.legendDiv = $("<div class='wax-legend'></div>").append(legend);
+        layer.legendDiv = $("<div class='wax-legend'></div>").append(layer.legend);
         this.container.append(layer.legendDiv);
     },
 
     // Generate the legend URL from the layer tile URL.
     legendUrl: function(layer) {
         var url = layer.getURL(new OpenLayers.Bounds());
-        return url.replace(/\d+\/\d+\/\d+\.\w+/, 'legend.json');
+        return url.replace(/\d+\/\d+\/\d+\.\w+/, 'layer.json');
     }
 });
 
