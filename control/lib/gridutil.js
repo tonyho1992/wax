042aef0e32ed939849b60ba9f2549084d767d1aa
Young Hahn <young@developmentseed.org>
2011-02-23 15:45:32 -0500

    Update gridutil to support legend.json endpoint.

--- control/lib/gridutil.js
+++ control/lib/gridutil.js
@@ -65,10 +65,10 @@ wax.GridManager.prototype.getGrid = function(url, callback) {
   var formatter = this.getFormatter(this.formatterUrl(url), function(f) {
       var grid_tile = that.grid_tiles[url];
       // If formatter & grid are finished, callback with `GridInstance`
-      if (grid_tile) {
+      if (f && grid_tile) {
         callback(new wax.GridInstance(grid_tile, f));
       // The grid isn't downloading, so start a download request
-      } else if (grid_tile !== false) {
+      } else if (f && grid_tile !== false) {
         that.grid_tiles[url] = false;
         $.jsonp({
           url: that.tileDataUrl(url),
@@ -105,12 +105,12 @@ wax.GridManager.prototype.tileDataUrl = function(url) {
 
 // Simplistically derive the URL of the formatter function from a tile URL
 wax.GridManager.prototype.formatterUrl = function(url) {
-  return url.replace(/\d+\/\d+\/\d+\.\w+/, 'formatter.json');
+  return url.replace(/\d+\/\d+\/\d+\.\w+/, 'layer.json');
 };
 
 // Request and save a formatter, calling `formatterReadDone` when finished.
 wax.GridManager.prototype.getFormatter = function(formatter_url, callback) {
-  if (this.formatters[formatter_url]) {
+  if (typeof this.formatters[formatter_url] !== 'undefined') {
     callback(this.formatters[formatter_url]);
     return;
   }
@@ -142,7 +142,11 @@ wax.GridManager.prototype.readDone = function(data, code_string) {
 // - @param {Object} data
 // - @param {String} layer
 wax.GridManager.prototype.formatterReadDone = function(data, url, callback) {
-    this.formatters[url] = new wax.Formatter(data);
+    if (data && data.formatter) {
+        this.formatters[url] = new wax.Formatter(data);
+    } else {
+        this.formatters[url] = false;
+    }
     callback(this.formatters[url]);
 };
 
