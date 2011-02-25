453e4db47ba3148b91ca684dd68903550b109d22
Tom MacWright <macwright@gmail.com>
2011-02-25 12:35:30 -0500

    Cleaning up google demo

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
 doc:
-	docco control/*.js lib/openlayers_wax.js
+	docco control/ol/*.js control/g/*.js
+
+build/wax.ol.min.js:
+	cat lib/*.js control/ol/*.js > build/wax.ol.js
+	uglifyjs build/wax.ol.js > build/wax.ol.min.js
+
+build/wax.g.min.js:
+	cat lib/*.js control/g/*.js connectors/g/*.js > build/wax.g.js
+	uglifyjs build/wax.g.js > build/wax.g.min.js
+
+build: build/wax.ol.min.js build/wax.g.min.js
--- example/google_interaction.js
+++ example/google_interaction.js
@@ -1,21 +1,19 @@
 // Google Maps Interaction Example
 var map;
 $(window).load(function() {
-    var l = new wax.g.MapType();
     map = new google.maps.Map(
         document.getElementById("google-canvas"), {
             center: new google.maps.LatLng(0, 0),
             zoom: 2,
             mapTypeControlOptions: {
-                mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'mapbox']
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP]
             }
         }
     );
 
-    map.mapTypes.set('mb', l);
-
-    // Set the default map to world light
-    map.setMapTypeId('mb');
+    // Create an assign the new maptype
+    map.mapTypes.set('mapbox', new wax.g.MapType());
+    map.setMapTypeId('mapbox');
 
     wax.g.Interaction(map);
     wax.g.Legend(map);
