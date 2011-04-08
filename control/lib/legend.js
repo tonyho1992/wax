46b8a1d6d93e57f2ad056005f4df9d8c6c614f35
Tom MacWright <macwright@gmail.com>
2011-04-08 12:12:36 -0400

    Replace that with proxy

--- control/lib/legend.js
+++ control/lib/legend.js
@@ -14,17 +14,16 @@ wax.Legend = function(context, container) {
 wax.Legend.prototype.render = function(urls) {
     $('.wax-legend', this.container).hide();
 
-    var that = this;
-    var render = function(content) {
+    var render = $.proxy(function(content) {
         if (!content) {
-            that.legends[url] = false;
-        } else if (that.legends[url]) {
-            that.legends[url].show();
+            this.legends[url] = false;
+        } else if (this.legends[url]) {
+            this.legends[url].show();
         } else {
-            that.legends[url] = $("<div class='wax-legend'></div>").append(content);
-            that.container.append(that.legends[url]);
+            this.legends[url] = $("<div class='wax-legend'></div>").append(content);
+            this.container.append(this.legends[url]);
         }
-    };
+    }, this);
     for (var i = 0; i < urls.length; i++) {
         var url = this.legendUrl(urls[i]);
         wax.request.get(url, function(data) {
