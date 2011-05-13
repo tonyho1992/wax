2d5cf53d23220ab7a448c81c88652773d0261afb
Tom MacWright <macwright@gmail.com>
2011-05-13 17:26:35 -0400

    Making zoombox removable

--- control/mm/zoombox.js
+++ control/mm/zoombox.js
@@ -27,69 +27,72 @@ com.modestmaps.Map.prototype.zoombox = function(opts) {
     var mouseDownPoint = null;
     var map = this;
 
-    var zoombox = {
-        map: this,
-        getMousePoint: function(e) {
-            // start with just the mouse (x, y)
-            var point = new com.modestmaps.Point(e.clientX, e.clientY);
-            // correct for scrolled document
-            point.x += document.body.scrollLeft + document.documentElement.scrollLeft;
-            point.y += document.body.scrollTop + document.documentElement.scrollTop;
+    var zoombox = this.zoombox;
 
-            // correct for nested offsets in DOM
-            for (var node = this.map.parent; node; node = node.offsetParent) {
-                point.x -= node.offsetLeft;
-                point.y -= node.offsetTop;
-            }
-            return point;
-        },
-        mouseDown: function(e) {
-            if (e.shiftKey) {
-                mouseDownPoint = zoombox.getMousePoint(e);
+    this.zoombox.remove = function() {
+        boxDiv.parentNode.removeChild(boxDiv);
+        map.removeCallback('mousedown', zoombox.mouseDown);
+    };
+    this.zoombox.getMousePoint = function(e) {
+        // start with just the mouse (x, y)
+        var point = new com.modestmaps.Point(e.clientX, e.clientY);
+        // correct for scrolled document
+        point.x += document.body.scrollLeft + document.documentElement.scrollLeft;
+        point.y += document.body.scrollTop + document.documentElement.scrollTop;
 
-                box.style.left = mouseDownPoint.x + 'px';
-                box.style.top = mouseDownPoint.y + 'px';
+        // correct for nested offsets in DOM
+        for (var node = map.parent; node; node = node.offsetParent) {
+            point.x -= node.offsetLeft;
+            point.y -= node.offsetTop;
+        }
+        return point;
+    };
+    this.zoombox.mouseDown = function(e) {
+        if (e.shiftKey) {
+            mouseDownPoint = zoombox.getMousePoint(e);
 
-                com.modestmaps.addEvent(map.parent, 'mousemove', zoombox.mouseMove);
-                com.modestmaps.addEvent(map.parent, 'mouseup', zoombox.mouseUp);
+            box.style.left = mouseDownPoint.x + 'px';
+            box.style.top = mouseDownPoint.y + 'px';
 
-                map.parent.style.cursor = 'crosshair';
-                return com.modestmaps.cancelEvent(e);
-            }
-        },
-        mouseMove: function(e) {
-            var point = zoombox.getMousePoint(e);
-            box.style.display = 'block';
-            if (point.x < mouseDownPoint.x) {
-                box.style.left = point.x + 'px';
-            } else {
-                box.style.left = mouseDownPoint.x + 'px';
-            }
-            box.style.width = Math.abs(point.x - mouseDownPoint.x) + 'px';
-            if (point.y < mouseDownPoint.y) {
-                box.style.top = point.y + 'px';
-            } else {
-                box.style.top = mouseDownPoint.y + 'px';
-            }
-            box.style.height = Math.abs(point.y - mouseDownPoint.y) + 'px';
-            return com.modestmaps.cancelEvent(e);
-        },
-        mouseUp: function(e) {
-            var point = zoombox.getMousePoint(e);
+            com.modestmaps.addEvent(map.parent, 'mousemove', zoombox.mouseMove);
+            com.modestmaps.addEvent(map.parent, 'mouseup', zoombox.mouseUp);
 
-            var l1 = map.pointLocation(point),
-                l2 = map.pointLocation(mouseDownPoint);
-
-            map.setExtent([l1, l2]);
-
-            box.style.display = 'none';
-            com.modestmaps.removeEvent(map.parent, 'mousemove', zoombox.mouseMove);
-            com.modestmaps.removeEvent(map.parent, 'mouseup', zoombox.mouseUp);
-
-            map.parent.style.cursor = 'auto';
-
+            map.parent.style.cursor = 'crosshair';
             return com.modestmaps.cancelEvent(e);
         }
+    };
+    this.zoombox.mouseMove = function(e) {
+        var point = zoombox.getMousePoint(e);
+        box.style.display = 'block';
+        if (point.x < mouseDownPoint.x) {
+            box.style.left = point.x + 'px';
+        } else {
+            box.style.left = mouseDownPoint.x + 'px';
+        }
+        box.style.width = Math.abs(point.x - mouseDownPoint.x) + 'px';
+        if (point.y < mouseDownPoint.y) {
+            box.style.top = point.y + 'px';
+        } else {
+            box.style.top = mouseDownPoint.y + 'px';
+        }
+        box.style.height = Math.abs(point.y - mouseDownPoint.y) + 'px';
+        return com.modestmaps.cancelEvent(e);
+    };
+    this.zoombox.mouseUp = function(e) {
+        var point = zoombox.getMousePoint(e);
+
+        var l1 = map.pointLocation(point),
+            l2 = map.pointLocation(mouseDownPoint);
+
+        map.setExtent([l1, l2]);
+
+        box.style.display = 'none';
+        com.modestmaps.removeEvent(map.parent, 'mousemove', zoombox.mouseMove);
+        com.modestmaps.removeEvent(map.parent, 'mouseup', zoombox.mouseUp);
+
+        map.parent.style.cursor = 'auto';
+
+        return com.modestmaps.cancelEvent(e);
     };
 
     com.modestmaps.addEvent(boxDiv, 'mousedown', zoombox.mouseDown);
