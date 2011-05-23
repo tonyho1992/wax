a068f698abbb5a388916f39029513fe860f34fc4
Tom MacWright <macwright@gmail.com>
2011-05-23 15:27:19 -0400

    Finishing up port of interaction

--- control/lib/tooltip.js
+++ control/lib/tooltip.js
@@ -10,24 +10,26 @@ wax.tooltip = {};
 // Get the active tooltip for a layer or create a new one if no tooltip exists.
 // Hide any tooltips on layers underneath this one.
 wax.tooltip.getToolTip = function(feature, context, index, evt) {
-    var tooltip = $(context).children('div.wax-tooltip-' +
-        index +
-        ':not(.removed)');
-    if (tooltip.size() === 0) {
-        tooltip = $("<div class='wax-tooltip wax-tooltip-" +
-            index +
-            "'>" +
-            '</div>').html(feature);
-        if (!$(context).triggerHandler('addedtooltip', [tooltip, context, evt])) {
-            $(context).append(tooltip);
-        }
-    }
-    for (var i = (index - 1); i > 0; i--) {
-        var fallback = $('div.wax-tooltip-' + i + ':not(.removed)');
-        if (fallback.size() > 0) {
-            fallback.addClass('hidden').hide();
-        }
-    }
+    // var tooltip = $(context).children('div.wax-tooltip-' +
+    //     index +
+    //     ':not(.removed)');
+
+    // if (tooltip.size() === 0) {
+    tooltip = document.createElement('div');
+    tooltip.className = 'wax-tooltip wax-tooltip-' + index;
+    tooltip.innerHTML = feature;
+
+        // if (!$(context).triggerHandler('addedtooltip', [tooltip, context, evt])) {
+             context.appendChild(tooltip);
+        // }
+    // }
+
+    // for (var i = (index - 1); i > 0; i--) {
+    //     var fallback = $('div.wax-tooltip-' + i + ':not(.removed)');
+    //     if (fallback.size() > 0) {
+    //         fallback.addClass('hidden').hide();
+    //     }
+    // }
     return tooltip;
 };
 
@@ -35,17 +37,17 @@ wax.tooltip.getToolTip = function(feature, context, index, evt) {
 // shown until this popup is closed or another popup is opened.
 wax.tooltip.click = function(feature, context, index) {
     var tooltip = wax.tooltip.getToolTip(feature, context, index);
-    var close = $('<a href="#close" class="close">Close</a>');
-    close.click(function() {
-        tooltip
-            .addClass('removed')
-            .fadeOut('fast', function() { $(this).remove(); });
+    var close = document.createElement('a');
+    close.href = '#close';
+    close.className = 'close';
+    close.innerHTML = 'Close';
+    close.addListener('click', function() {
+        tooltip.parentNode.removeChild(tooltip);
         return false;
     });
-    tooltip
-        .addClass('wax-popup')
-        .html(feature)
-        .append(close);
+    tooltip.className += ' wax-popup';
+    tooltip.innerHTML = feature;
+    tooltip.appendChild(close);
 };
 
 // Show a tooltip.
@@ -53,15 +55,14 @@ wax.tooltip.select = function(feature, context, layer_id, evt) {
     if (!feature) return;
 
     wax.tooltip.getToolTip(feature, context, layer_id, evt);
-    $(context).css('cursor', 'pointer');
-    $('div', context).css('cursor', 'pointer');
+    context.style.cursor = 'pointer';
 };
 
 // Hide all tooltips on this layer and show the first hidden tooltip on the
 // highest layer underneath if found.
 wax.tooltip.unselect = function(feature, context, layer_id, evt) {
-    $(context)
-        .css('cursor', 'default');
+    context.style.cursor = 'default';
+    /*
     if (layer_id) {
         $('div.wax-tooltip-' + layer_id + ':not(.wax-popup)')
             .remove();
@@ -69,13 +70,13 @@ wax.tooltip.unselect = function(feature, context, layer_id, evt) {
         $('div.wax-tooltip:not(.wax-popup)')
             .remove();
     }
+    */
 
     // TODO: remove
-    $('div', context).css('cursor', 'default');
 
-    $('div.wax-tooltip:first')
-        .removeClass('hidden')
-        .show();
+    // $('div.wax-tooltip:first')
+    //     .removeClass('hidden')
+    //     .show();
 
-    $(context).triggerHandler('removedtooltip', [feature, context, evt]);
+    // $(context).triggerHandler('removedtooltip', [feature, context, evt]);
 };
