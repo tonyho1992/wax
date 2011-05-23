d686385673da911653672ed9ec6d884496babe86
Tom MacWright <macwright@gmail.com>
2011-05-23 18:37:49 -0400

    ...

--- manual/site.js
+++ manual/site.js
@@ -4,7 +4,7 @@ $.domReady(function() {
     var nav = $('.navigation ul');
     $('.md').each(function() {
         var html = document.createElement('div');
-        html.innerHTML = (new Showdown.converter()).makeHtml(this.innerHTML);
+        $(html).html(new Showdown.converter()).makeHtml($(this).html());
         html.className = this.className;
         html.id = this.id;
         // TODO: free from shackles of jQuery
