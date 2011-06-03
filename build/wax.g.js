973e0defaca645834ae97344b0c3b0c1428d7c60
Young Hahn <young@developmentseed.org>
2011-06-03 16:09:00 -0400

    Remove legacy handling for google controls.

--- build/wax.g.js
+++ build/wax.g.js
@@ -810,23 +810,6 @@ wax.g.interaction = function(map, options) {
     return interaction.add(map);
 };
 
-// Wrapper to support legacy Controls object interface.
-wax = wax || {};
-
-wax.g = wax.g || {};
-
-wax.g.Controls = function(map) {
-    this.map = map;
-};
-
-wax.g.Controls.prototype.legend = function(options) {
-    return wax.g.legend(this.map, options);
-};
-
-wax.g.Controls.prototype.interaction = function(options) {
-    return wax.g.interaction(this.map, options);
-};
-
 wax = wax || {};
 wax.g = wax.g || {};
 
