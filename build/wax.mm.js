cc28f22c70eda92451349d6dabb11983b7a6ef5c
Young Hahn <young@developmentseed.org>
2011-06-03 14:39:37 -0400

    Refactor tooltips to use transitions, handle click/select exclusion.

--- build/wax.mm.js
+++ build/wax.mm.js
@@ -459,34 +459,24 @@ var w = function(self) {
     };
     return self;
 };
-// Requires: jQuery
-//
-// Wax GridUtil
-// ------------
-
-// Wax header
 var wax = wax || {};
 wax.tooltip = {};
 
-// TODO: make this a non-global
-var _currentTooltip;
-
-var waxRemoveTooltip = function() {
-    this.parentNode.removeChild(this);
-    if (_currentTooltip) {
-        _currentTooltip = undefined;
-    }
-};
-
 wax.tooltip = function(options) {
+    this._currentTooltip = undefined;
     options = options || {};
     if (options.animationOut) this.animationOut = options.animationOut;
     if (options.animationIn) this.animationIn = options.animationIn;
 };
 
+// Helper function to determine whether a given element is a wax popup.
+wax.tooltip.prototype.isPopup = function(el) {
+    return el && el.className.indexOf('wax-popup') !== -1;
+};
+
 // Get the active tooltip for a layer or create a new one if no tooltip exists.
 // Hide any tooltips on layers underneath this one.
-wax.tooltip.prototype.getToolTip = function(feature, context, index, evt) {
+wax.tooltip.prototype.getTooltip = function(feature, context, index, evt) {
     tooltip = document.createElement('div');
     tooltip.className = 'wax-tooltip wax-tooltip-' + index;
     tooltip.innerHTML = feature;
@@ -494,27 +484,66 @@ wax.tooltip.prototype.getToolTip = function(feature, context, index, evt) {
     return tooltip;
 };
 
+// Hide a given tooltip.
+wax.tooltip.prototype.hideTooltip = function(el) {
+    if (!el) return;
+    var event;
+    var remove = function() {
+        this.parentNode.removeChild(this);
+    };
+    if (el.style['-webkit-transition'] !== undefined && this.animationOut) {
+        event = 'webkitTransitionEnd';
+    } else if (el.style.MozTransition !== undefined && this.animationOut) {
+        event = 'transitionend';
+    }
+    if (event) {
+        el.addEventListener(event, remove, false);
+        el.addEventListener('transitionend', remove, false);
+        el.className += ' ' + this.animationOut;
+    } else {
+        el.parentNode.removeChild(el);
+    }
+};
+
 // Expand a tooltip to be a "popup". Suspends all other tooltips from being
 // shown until this popup is closed or another popup is opened.
 wax.tooltip.prototype.click = function(feature, context, index) {
-    var tooltip = this.getToolTip(feature, context, index);
+    // Hide any current tooltips.
+    this.unselect(context);
+
+    var tooltip = this.getTooltip(feature, context, index);
     var close = document.createElement('a');
     close.href = '#close';
     close.className = 'close';
     close.innerHTML = 'Close';
-    close.addListener('click', function() {
-        tooltip.parentNode.removeChild(tooltip);
+
+    var closeClick = wax.util.bind(function(ev) {
+        this.hideTooltip(tooltip);
+        this._currentTooltip = undefined;
+        ev.returnValue = false; // Prevents hash change.
+        if (ev.stopPropagation) ev.stopPropagation();
+        if (ev.preventDefault) ev.preventDefault();
         return false;
-    });
+    }, this);
+    // IE compatibility.
+    if (close.addEventListener) {
+        close.addEventListener('click', closeClick, false);
+    } else if (close.attachEvent) {
+        close.attachEvent('onclick', closeClick);
+    }
+
     tooltip.className += ' wax-popup';
     tooltip.innerHTML = feature;
     tooltip.appendChild(close);
+    this._currentTooltip = tooltip;
 };
 
 // Show a tooltip.
 wax.tooltip.prototype.select = function(feature, context, layer_id, evt) {
     if (!feature) return;
-    _currentTooltip = this.getToolTip(feature, context, layer_id, evt);
+    if (this.isPopup(this._currentTooltip)) return;
+
+    this._currentTooltip = this.getTooltip(feature, context, layer_id, evt);
     context.style.cursor = 'pointer';
 };
 
@@ -522,17 +551,12 @@ wax.tooltip.prototype.select = function(feature, context, layer_id, evt) {
 // Hide all tooltips on this layer and show the first hidden tooltip on the
 // highest layer underneath if found.
 wax.tooltip.prototype.unselect = function(context) {
+    if (this.isPopup(this._currentTooltip)) return;
+
     context.style.cursor = 'default';
-    if (_currentTooltip) {
-        // In WebKit browsers, support nice CSS animations.
-        // This is possible in -moz browsers but will need writing.
-        if (_currentTooltip.style['-webkit-animationName'] !== undefined && this.animationOut) {
-            _currentTooltip.addEventListener('webkitAnimationEnd', waxRemoveTooltip, false);
-            _currentTooltip.className += ' ' + this.animationOut;
-        } else {
-            _currentTooltip.parentNode.removeChild(_currentTooltip);
-            _currentTooltip = undefined;
-        }
+    if (this._currentTooltip) {
+        this.hideTooltip(this._currentTooltip);
+        this._currentTooltip = undefined;
     }
 };
 
@@ -1112,7 +1136,7 @@ wax.interaction = function(map, options) {
                             if (feature) {
                                 switch (this.clickAction) {
                                     case 'full':
-                                        this.callbacks.click(feature, this.parent, 0, evt);
+                                        this.callbacks.click(feature, map.parent, 0, evt);
                                         break;
                                     case 'location':
                                         window.location = feature;
