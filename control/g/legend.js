295b1ab22820ba3f9cb3b194b12768ab1085a0f6
Will White <will@developmentseed.org>
2011-07-08 17:21:53 -0400

    Update legend control for Google Maps.

--- control/g/legend.js
+++ control/g/legend.js
@@ -1,34 +1,28 @@
 wax = wax || {};
 wax.g = wax.g || {};
 
+// Legend Control
+// --------------
 // Adds legends to a google Map object.
-wax.g.legend = function(map, options) {
+wax.g.legend = function(options) {
     options = options || {};
-    var legend = {
-        add: function() {
-            var url;
-            this.legend = new wax.Legend(map.getDiv(), options.container);
-            // Ideally we would use the 'tilesloaded' event here. This doesn't seem to
-            // work so we use the much less appropriate 'idle' event.
-            google.maps.event.addListener(map, 'idle', wax.util.bind(function() {
-                if (url) return;
+    var l, // parent legend
+        legend = {};
 
-                // Get a tile URL for each relevant layer, from which legend URLs
-                // are derived.
-                url = [];
-                for (var i in map.mapTypes) {
-                    if (!map.mapTypes[i].interactive) continue;
-                    var mapType = map.mapTypes[i];
-                    for (var key in mapType.cache) {
-                        url.push(mapType.cache[key].src);
-                        break;
-                    }
-                };
-                url.length && this.legend.render(url);
-            }, this));
-            return this;
-        }
+    legend.add = function() {
+        l = wax.legend()
+            .content(options.legend || '');
+        return this;
     };
-    return legend.add(map);
-};
 
+    legend.element = function() {
+        return l.element();
+    };
+
+    legend.appendTo = function(elem) {
+        wax.util.$(elem).appendChild(l.element());
+        return this;
+    };
+
+    return legend.add();
+};
