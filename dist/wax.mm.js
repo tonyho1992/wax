5e8f23b794129cf99e7f08f1b42653947d915cf4
Tom MacWright <tom@macwright.org>
2011-09-01 12:08:20 -0400

    Tooltip code testing stub

--- dist/wax.mm.js
+++ dist/wax.mm.js
@@ -1,4 +1,4 @@
-/* wax - 3.0.5 - 1.0.4-361-g59892a1 */
+/* wax - 3.0.5 - 1.0.4-367-g12e3b2e */
 
 
 /*!
@@ -601,106 +601,106 @@ var wax = wax || {};
 wax.tooltip = {};
 
 wax.tooltip = function(options) {
-    this._currentTooltip = undefined;
     options = options || {};
-    if (options.animationOut) this.animationOut = options.animationOut;
-    if (options.animationIn) this.animationIn = options.animationIn;
-};
 
-// Helper function to determine whether a given element is a wax popup.
-wax.tooltip.prototype.isPopup = function(el) {
-    return el && el.className.indexOf('wax-popup') !== -1;
-};
+    var tooltip = {},
+        animationOut = options.animationOut,
+        animationIn = options.animationIn;
 
-// Get the active tooltip for a layer or create a new one if no tooltip exists.
-// Hide any tooltips on layers underneath this one.
-wax.tooltip.prototype.getTooltip = function(feature, context, index, evt) {
-    tooltip = document.createElement('div');
-    tooltip.className = 'wax-tooltip wax-tooltip-' + index;
-    tooltip.innerHTML = feature;
-    context.appendChild(tooltip);
-    return tooltip;
-};
-
-// Hide a given tooltip.
-wax.tooltip.prototype.hideTooltip = function(el) {
-    if (!el) return;
-    var event;
-    var remove = function() {
-        if (this.parentNode) this.parentNode.removeChild(this);
-    };
-    if (el.style['-webkit-transition'] !== undefined && this.animationOut) {
-        event = 'webkitTransitionEnd';
-    } else if (el.style.MozTransition !== undefined && this.animationOut) {
-        event = 'transitionend';
+    // Helper function to determine whether a given element is a wax popup.
+    function isPopup(el) {
+        return el && el.className.indexOf('wax-popup') !== -1;
     }
-    if (event) {
-        el.addEventListener(event, remove, false);
-        el.addEventListener('transitionend', remove, false);
-        el.className += ' ' + this.animationOut;
-    } else {
-        if (el.parentNode) el.parentNode.removeChild(el);
+
+    // Get the active tooltip for a layer or create a new one if no tooltip exists.
+    // Hide any tooltips on layers underneath this one.
+    function getTooltip(feature, context, index, evt) {
+        tooltip = document.createElement('div');
+        tooltip.className = 'wax-tooltip wax-tooltip-' + index;
+        tooltip.innerHTML = feature;
+        context.appendChild(tooltip);
+        return tooltip;
     }
-};
 
-// Expand a tooltip to be a "popup". Suspends all other tooltips from being
-// shown until this popup is closed or another popup is opened.
-wax.tooltip.prototype.click = function(feature, context, index) {
-    // Hide any current tooltips.
-    this.unselect(context);
-
-    var tooltip = this.getTooltip(feature, context, index);
-    var close = document.createElement('a');
-    close.href = '#close';
-    close.className = 'close';
-    close.innerHTML = 'Close';
-
-    var closeClick = wax.util.bind(function(ev) {
-        this.hideTooltip(tooltip);
-        this._currentTooltip = undefined;
+    function closePopup() {
+        hideTooltip(tooltip);
+        _currentTooltip = undefined;
         ev.returnValue = false; // Prevents hash change.
         if (ev.stopPropagation) ev.stopPropagation();
         if (ev.preventDefault) ev.preventDefault();
         return false;
-    }, this);
-    // IE compatibility.
-    if (close.addEventListener) {
-        close.addEventListener('click', closeClick, false);
-    } else if (close.attachEvent) {
-        close.attachEvent('onclick', closeClick);
     }
 
-    tooltip.className += ' wax-popup';
-    tooltip.innerHTML = feature;
-    tooltip.appendChild(close);
-    this._currentTooltip = tooltip;
-};
-
-// Show a tooltip.
-wax.tooltip.prototype.select = function(feature, context, layer_id, evt) {
-    if (!feature) return;
-    if (this.isPopup(this._currentTooltip)) return;
-
-    this._currentTooltip = this.getTooltip(feature, context, layer_id, evt);
-    context.style.cursor = 'pointer';
-};
-
-
-// Hide all tooltips on this layer and show the first hidden tooltip on the
-// highest layer underneath if found.
-wax.tooltip.prototype.unselect = function(context) {
-    if (this.isPopup(this._currentTooltip)) return;
-
-    context.style.cursor = 'default';
-    if (this._currentTooltip) {
-        this.hideTooltip(this._currentTooltip);
-        this._currentTooltip = undefined;
+    // Hide a given tooltip.
+    function hideTooltip(el) {
+        if (!el) return;
+        var event;
+        var remove = function() {
+            if (this.parentNode) this.parentNode.removeChild(this);
+        };
+        if (el.style['-webkit-transition'] !== undefined && animationOut) {
+            event = 'webkitTransitionEnd';
+        } else if (el.style.MozTransition !== undefined && animationOut) {
+            event = 'transitionend';
+        }
+        if (event) {
+            el.addEventListener(event, remove, false);
+            el.addEventListener('transitionend', remove, false);
+            el.className += ' ' + animationOut;
+        } else {
+            if (el.parentNode) el.parentNode.removeChild(el);
+        }
     }
-};
 
-wax.tooltip.prototype.out = wax.tooltip.prototype.unselect;
-wax.tooltip.prototype.over = wax.tooltip.prototype.select;
-wax.tooltip.prototype.click = wax.tooltip.prototype.click;
+    // Show a tooltip.
+    tooltip.select = function(feature, context, layer_id, evt) {
+        if (!feature) return;
+        if (isPopup(_currentTooltip)) return;
+
+        _currentTooltip = getTooltip(feature, context, layer_id, evt);
+        context.style.cursor = 'pointer';
+    };
+
+    // Hide all tooltips on this layer and show the first hidden tooltip on the
+    // highest layer underneath if found.
+    tooltip.unselect = function(context) {
+        if (isPopup(_currentTooltip)) return;
+
+        context.style.cursor = 'default';
+        if (_currentTooltip) {
+            hideTooltip(_currentTooltip);
+            _currentTooltip = undefined;
+        }
+    };
+
+    // Expand a tooltip to be a "popup". Suspends all other tooltips from being
+    // shown until this popup is closed or another popup is opened.
+    tooltip.click = function(feature, context, index) {
+        // Hide any current tooltips.
+        this.unselect(context);
+
+        var tooltip = getTooltip(feature, context, index);
+        var close = document.createElement('a');
+        close.href = '#close';
+        close.className = 'close';
+        close.innerHTML = 'Close';
+
+
+        // IE compatibility.
+        if (close.addEventListener) {
+            close.addEventListener('click', closeClick, false);
+        } else if (close.attachEvent) {
+            close.attachEvent('onclick', closeClick);
+        }
+
+        tooltip.className += ' wax-popup';
+        tooltip.innerHTML = feature;
+        tooltip.appendChild(close);
+        _currentTooltip = tooltip;
+    };
+
+    return tooltip;
+};
 var wax = wax || {};
 wax.util = wax.util || {};
 
@@ -1043,14 +1043,16 @@ wax.mm = wax.mm || {};
 // control. This function can be used chaining-style with other
 // chaining-style controls.
 wax.mm.fullscreen = function(map) {
-    var state = 1,
+    // true: fullscreen
+    // false: minimized
+    var state = false,
         fullscreen = {},
         a,
         smallSize;
 
     function click(e) {
         if (e) com.modestmaps.cancelEvent(e);
-        if (state = !state) {
+        if (state) {
             fullscreen.original();
         } else {
             fullscreen.full();
@@ -1069,6 +1071,7 @@ wax.mm.fullscreen = function(map) {
         return this;
     };
     fullscreen.full = function() {
+        if (state) { return; } else { state = true; }
         smallSize = [map.parent.offsetWidth, map.parent.offsetHeight];
         map.parent.className += ' wax-fullscreen-map';
         map.setSize(
@@ -1076,6 +1079,7 @@ wax.mm.fullscreen = function(map) {
             map.parent.offsetHeight);
     };
     fullscreen.original = function() {
+        if (!state) { return; } else { state = false; }
         map.parent.className = map.parent.className.replace('wax-fullscreen-map', '');
         map.setSize(
             smallSize[0],
@@ -1359,7 +1363,7 @@ wax.mm.interaction = function(map, tilejson, options) {
         _downLock = true;
         _d = wax.util.eventoffset(e);
         if (e.type === 'mousedown') {
-            MM.addEvent(map.parent, 'mouseup', onUp);
+            MM.addEvent(document.body, 'mouseup', onUp);
 
         // Only track single-touches. Double-touches will not affect this
         // control
