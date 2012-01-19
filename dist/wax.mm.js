f3771f4ab61e7280c913aeefea8126dce3726447
Tom MacWright <tom@macwright.org>
2012-01-19 14:31:42 -0500

    broken for the time being

--- dist/wax.mm.js
+++ dist/wax.mm.js
@@ -1,4 +1,4 @@
-/* wax - 5.0.0-alpha1 - 1.0.4-485-gf88b6e0 */
+/* wax - 5.0.0-alpha1 - 1.0.4-486-g1efbc38 */
 
 
 /*!
@@ -3514,29 +3514,37 @@ wax.mm = wax.mm || {};
 
 // A layer connector for Modest Maps conformant to TileJSON
 // https://github.com/mapbox/tilejson
-wax.mm.connector = function(options) {
+wax.mm.connector = function(tj) {
     this.options = {
-        tiles: options.tiles,
-        scheme: options.scheme || 'xyz',
-        minzoom: options.minzoom || 0,
-        maxzoom: options.maxzoom || 22,
-        bounds: options.bounds || [-180, -90, 180, 90]
+        tiles: tj.tiles,
+        scheme: tj.scheme || 'xyz',
+        minzoom: tj.minzoom || 0,
+        maxzoom: tj.maxzoom || 22,
+        bounds: tj.bounds || [-180, -90, 180, 90]
     };
+
+    if (tj.bounds[0] == -180 && tj.bounds[1] == -90 &&
+        tj.bounds[2] == 180 && tj.bounds[3] == 90) {
+        this.tileLimits = [
+            new MM.Coordinate(0,0,0).zoomTo(tj.minzoom),             // top left outer
+            new MM.Coordinate(1,1,0).zoomTo(tj.maxzoom)
+        ];
+    } else {
+        var p = new MM.MercatorProjection(0,
+          MM.deriveTransformation(-Math.PI,  Math.PI, 0, 0,
+               Math.PI,  Math.PI, 1, 0,
+              -Math.PI, -Math.PI, 0, 1));
+
+        this.tileLimits = [
+            p.locationCoordinate(
+                new MM.Location(tj.bounds[1], tj.bounds[0])).zoomTo(tj.minzoom),
+            p.locationCoordinate(
+                new MM.Location(tj.bounds[3], tj.bounds[2])).zoomTo(tj.maxzoom)
+        ];
+    }
 };
 
 wax.mm.connector.prototype = {
-    outerLimits: function() {
-        return [
-            this.locationCoordinate(
-                new MM.Location(
-                    this.options.bounds[0],
-                    this.options.bounds[1])).zoomTo(this.options.minzoom),
-            this.locationCoordinate(
-                new MM.Location(
-                    this.options.bounds[2],
-                    this.options.bounds[3])).zoomTo(this.options.maxzoom)
-        ];
-    },
     getTile: function(c) {
         if (!(coord = this.sourceCoordinate(c))) return null;
 
@@ -3557,3 +3565,14 @@ wax.mm.connector.prototype = {
 if (MM) {
     MM.extend(wax.mm.connector, MM.MapProvider);
 }
+
+wax.mm.limit = function(tj) {
+    var p = new MM.MercatorProjection(0);
+
+    return [
+        p.locationCoordinate(
+            new MM.Location(tj.bounds[1], tj.bounds[0])).zoomTo(tj.minzoom),
+        p.locationCoordinate(
+            new MM.Location(tj.bounds[3], tj.bounds[2])).zoomTo(tj.maxzoom)
+    ];
+};
