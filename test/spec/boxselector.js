c719d3895bd63013e5b9d00fd84579da2e53770d
Young Hahn <young@developmentseed.org>
2012-11-25 11:57:41 -0500

    Update test for boxselector. Refs #268.

--- test/spec/boxselector.js
+++ test/spec/boxselector.js
@@ -10,10 +10,9 @@ describe('boxselector', function() {
         map = new com.modestmaps.Map(div, new com.modestmaps.TemplatedMapProvider(
             'http://{S}tile.openstreetmap.org/{Z}/{X}/{Y}.png', ['a.']));
         map.setCenterZoom(new com.modestmaps.Location(37.811530, -122.2666097), 10);
-        boxselector = wax.mm.boxselector(map, {}, {
-            callback: function() {
-                callbackResult = arguments;
-            }
+        boxselector = wax.mm.boxselector().map(map).add();
+        boxselector.addCallback('change', function() {
+            callbackResult = arguments;
         });
     });
 
@@ -26,11 +25,11 @@ describe('boxselector', function() {
         });
         waits(100);
         runs(function() {
-            expect(callbackResult.length).toEqual(1);
-            expect(callbackResult[0][0].lat).toEqual(30);
-            expect(callbackResult[0][0].lon).toEqual(20);
-            expect(callbackResult[0][1].lat).toEqual(10);
-            expect(callbackResult[0][1].lon).toEqual(40);
+            expect(callbackResult.length).toEqual(2);
+            expect(callbackResult[1][0].lat).toEqual(30);
+            expect(callbackResult[1][0].lon).toEqual(20);
+            expect(callbackResult[1][1].lat).toEqual(10);
+            expect(callbackResult[1][1].lon).toEqual(40);
         });
     });
 });
